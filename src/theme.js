efc12f3ebe03c98434640d878db0807f5f60fe26
Harry <hsharma@primoko.com>
2018-07-19 17:59:25 +0530

    Banner

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import indigo from '@material-ui/core/colors/indigo';
 import pink from '@material-ui/core/colors/pink';
 import red from '@material-ui/core/colors/red';
+import 'typeface-lato'
 
 // All the following keys are optional.
 // We try our best to provide a great default value.
