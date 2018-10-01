cbe8cdda726db0ad3ae35eb043e4dc49b314080f
Harry <hsharma@primoko.com>
2018-10-02 00:06:03 +0530

    about page restored

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Content, { HTMLContent } from '../components/Content';
+import TextImageWidget from '../components/TextImageWidget';
 import { withStyles } from '@material-ui/core/styles';
+import Quote from '../components/Quotes';
 import { withPrefix } from 'gatsby-link';
-import Features from '../components/Features'
-import Testimonials from '../components/Testimonials'
-import Pricing from '../components/Pricing'
-
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '../components/Button';
+import Hidden from '@material-ui/core/Hidden';
+import JoinTeam from '../components/JoinTeam';
+import Link from 'gatsby-link';
 const styles = (theme) => ({
 	banner: {
 		position: 'relative',
@@ -76,88 +81,41 @@ export const AboutPageTemplate = ({
 	classes = classes || {};
 
 	return (
-		<section className="section section--gradient">
-			<div className="container">
-				<div className="section">
-					<div className="columns">
-						<div className="column is-10 is-offset-1">
-							<div className="content">
-								<div
-									className="full-width-image-container margin-top-0"
-									style={{ backgroundImage: `url(${image})` }}
-								>
-									<h2
-										className="has-text-weight-bold is-size-1"
-										style={{
-											boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-											backgroundColor: '#f40',
-											color: 'white',
-											padding: '1rem'
-										}}
-									>
-										{title}
-									</h2>
+		<div>
+			<TextImageWidget title={title} heading={heading} />
+			<div className={classes.banner}>
+				<Hidden xsDown>
+					<Grid container direction="row" justify="flex-end" alignItems="center" className={classes.block}>
+						<Grid container justify="center" alignItems="center">
+							<Grid item>
+								<Typography className={classes.subTitle} align="center">
+									Join the team
+								</Typography>
+								<Typography className={classes.paragraph} align="center">
+									Are you a nerd looking for home? Or a nerd looking to work from home? Checkout our
+									current opportunities.
+								</Typography>
+								<div style={{ width: '100%', textAlign: 'center' }}>
+									<Button
+										styles={classes.button}
+										text="See career opportunities"
+										to="/careers"
+										component={Link}
+									/>
 								</div>
-								<div className="columns">
-									<div className="column is-7">
-										<h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
-										<p>{description}</p>
-									</div>
-								</div>
-								<Features gridItems={intro.blurbs} />
-								<div className="columns">
-									<div className="column is-7">
-										<h3 className="has-text-weight-semibold is-size-3">{main.heading}</h3>
-										<p>{main.description}</p>
-									</div>
-								</div>
-								<div className="tile is-ancestor">
-									<div className="tile is-vertical">
-										<div className="tile">
-											<div className="tile is-parent is-vertical">
-												<article className="tile is-child">
-													<img
-														style={{ borderRadius: '5px' }}
-														src={main.image1.image}
-														alt={main.image1.alt}
-													/>
-												</article>
-											</div>
-											<div className="tile is-parent">
-												<article className="tile is-child">
-													<img
-														style={{ borderRadius: '5px' }}
-														src={main.image2.image}
-														alt={main.image2.alt}
-													/>
-												</article>
-											</div>
-										</div>
-										<div className="tile is-parent">
-											<article className="tile is-child">
-												<img
-													style={{ borderRadius: '5px' }}
-													src={main.image3.image}
-													alt={main.image3.alt}
-												/>
-											</article>
-										</div>
-									</div>
-								</div>
-								<Testimonials testimonials={testimonials} />
-								<div
-									className="full-width-image-container"
-									style={{ backgroundImage: `url(${fullImage})` }}
-								/>
-								<h2 className="has-text-weight-semibold is-size-2">{pricing.heading}</h2>
-								<p className="is-size-5">{pricing.description}</p>
-								<Pricing data={pricing.plans} />
-							</div>
-						</div>
-					</div>
-				</div>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Hidden>
 			</div>
-		</section>
+			<Hidden smUp>
+				<JoinTeam />
+			</Hidden>
+			<Quote
+				author="Martin Fowler"
+				quote="Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+			/>
+		</div>
 	);
 };
 
